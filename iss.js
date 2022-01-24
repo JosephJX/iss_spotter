f57901cb4f5f569daf459e8bdba0242241ed1fdf
JosephJX <joseph00@my.yorku.ca>
2022-01-24 17:15:55 +0000

    returned coordinates based on ip address

---
+++ iss.js
@@ -0,0 +1,39 @@
+const request = require('request')
+
+const fetchMyIP = function (callback) {
+  request('https://api.ipify.org?format=json', (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      // const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(`Status Code ${response.statusCode} when fetching IP: ${body}`), null);
+      return;
+    }
+
+    const ip = JSON.parse(body).ip;
+    callback(null, ip);
+  });
+  // use request to fetch IP address from JSON API
+};
+const fetchCoordsByIP = function (ip, callback) {
+  request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    const { latitude, longitude } = JSON.parse(body);
+
+    callback(null, { latitude, longitude });
+
+  });
+};
+module.exports = { fetchMyIP, fetchCoordsByIP };
